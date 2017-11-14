0dfc6a9b3c239590e58c0ee225202173127ab740
ROCJOHNSONE560\rjohnson <rjohnson@healthgrades.com>
2017-11-14 16:12:17 -0500

    Add project files.

---
+++ scoreboardVB10/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="scoreboardVB10.MvcApplication" Language="C#" %>
---
+++ scoreboardVB10/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(scoreboardVB10.Startup))]
+namespace scoreboardVB10
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
