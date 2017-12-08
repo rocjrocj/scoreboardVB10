3faea148dc0c51ad518d13e42a9320a780d02b2b
ROCJOHNSONE560\rjohnson <rjohnson@healthgrades.com>
2017-12-08 13:20:42 -0500

    Friday changes

--- scoreboardVB10/ScoreboardVBHub.cs
+++ scoreboardVB10/ScoreboardVBHub.cs
@@ -4,17 +4,30 @@ using System.Linq;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace scoreboardVB10
 {
     public class SimpleHub : Hub
     {
-        //private static int _scoreVB = 0;
+        //private static string message = "Initial message";
         public void SendMessage(string message)
         {
             //_scoreVB += 1;
             //Clients.All.introduce(_scoreVB);
             Clients.All.showMessage(message);
+        }
+
+        public void JoinRoom(string roomName)
+        {
+            Groups.Add(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage(Context.User.Identity.Name + " joined" + roomName + ".");
+        }
+
+        public void LeaveRoom(string roomName)
+        {
+            Groups.Remove(Context.ConnectionId, roomName);
+            Clients.Group(roomName).addChatMessage(Context.User.Identity.Name + " left" + roomName + ".");
         }
     }
 }
